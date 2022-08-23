2f663453d2ab35c5fbd7459763cdb236e9feb75f
yanmiao99 <503084944@qq.com>
2022-08-23 21:05:33 +0800

    feat : 初始化项目

---
+++ README.md
@@ -0,0 +1,10 @@
+## 项目启动
+
+```sh
+pnpm install
+pnpm run dev
+pnpm run build
+```
+
+### 项目移动端适配 
+
---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    我是主页
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<style scoped>
+
+</style>
